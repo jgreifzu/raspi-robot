69f56a04a31e81292ce1da542fea6e562f45e47a
jgreifzu <johnpgreifzu@gmail.com>
2020-04-16 19:56:01 -0500

    things

--- raspi-pythonThings/serialComm.py
+++ raspi-pythonThings/serialComm.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
 import serial
+
+def readFromSerial():
+    try:
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+        # sometimes when reading from serial it gets a weird character that utf-8 cant decode
+    except UnicodeDecodeError:
+        print("error in reading")
+
+def sendMotorCommand(leftSpeed, rightSpeed):
+    s = "<L"+str(leftSpeed)+"R"+str(rightSpeed)+">"
+    #print(s)
+    ser.write(s.encode('utf-8'))
+
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
     ser.flush()
+    timer = 0
     while True:
-        ser.write(b"<L4R4>")
+        sendMotorCommand(5,6)
         if ser.in_waiting > 0:
-            try:
-                line = ser.readline().decode('utf-8').rstrip()
-                print(line)
-            # sometimes when reading from serial it gets a weird character that utf-8 cant decode
-            except UnicodeDecodeError:
-                print("error in reading")
+            readFromSerial()
+            
             
